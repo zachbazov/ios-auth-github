ea724eb56ea299d4662c51d05930802be6e170b6
Zach <zachbazov@gmail.com>
2020-06-16 12:41:50 +0300

    Initial Commit

---
+++ ios-auth-github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-auth-github
+//
+//  Created by Zach Bazov on 16/06/2020.
+//  Copyright © 2020 Zach Bazov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
