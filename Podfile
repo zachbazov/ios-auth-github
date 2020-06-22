c584cf68bbc7784b2b315328628507e9c60ba7d1
Zach <zachbazov@gmail.com>
2020-06-23 01:09:01 +0300

    Added pods

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '13.5'
+
+target 'ios-auth-github' do
+  use_frameworks!
+
+  pod 'SwiftKeychainWrapper'
+
+end
---
+++ Pods/Target Support Files/Pods-ios-auth-github/Pods-ios-auth-github-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_auth_github : NSObject
+@end
+@implementation PodsDummy_Pods_ios_auth_github
+@end
